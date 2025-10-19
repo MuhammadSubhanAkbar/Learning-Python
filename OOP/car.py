96097c60a5d40c60209c3bd1c058e2c79c3fc267
Subhan Akbar <subhanakbar346@gmail.com>
2025-10-19 20:36:44 +0500

    Learned methods in OOP

--- OOP/car.py
+++ OOP/car.py
@@ -1,7 +1,19 @@
 class Car:
-    def __init__(self, make, model, year, color, for_sale):
-        self.make = make
+    def __init__(self, maker, model, year, color, for_sale):
+        self.maker = maker
         self.model = model
         self.year = year
         self.color = color
         self.for_sale = for_sale
+
+    def drive(self):
+        print(f"You drive the  {self.year} {self.color} {self.maker} {self.model}.")  # Fixed: self.maker
+
+    def stop(self):
+        print(f"You stopped the {self.maker} {self.color} {self.model}.")   # Fixed: self.maker
+
+    def sale(self):
+        if self.for_sale:
+            print("ON SALE!!")
+        else:
+            print("NOT ON SALE.")
